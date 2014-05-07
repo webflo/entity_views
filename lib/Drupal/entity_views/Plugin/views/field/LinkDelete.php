8d81cab62b9c3cc8baf9d27b116e90ed09697591
Florian Weber <florian.weber@ueberbit.de>
2014-05-07 18:05:46 +0200

    Add entity_view module. Entity Views contains generic view integration for entities.

---
+++ lib/Drupal/entity_views/Plugin/views/field/LinkDelete.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\entity_views\Plugin\views\field\LinkDelete.
+ */
+
+namespace Drupal\entity_views\Plugin\views\field;
+
+use Drupal\node\Plugin\views\field\Link;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to present a link to delete a node.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @PluginID("entity_link_delete")
+ */
+class LinkDelete extends Link {
+
+  /**
+   * Prepares the link to delete a entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The node entity this field belongs to.
+   * @param \Drupal\views\ResultRow $values
+   *   The values retrieved from the view's result set.
+   *
+   * @return string
+   *   Returns a string for the link text.
+   */
+  protected function renderLink($entity, ResultRow $values) {
+    // Ensure user has access to delete this node.
+    if (!$entity->access('delete')) {
+      $this->options['alter']['make_link'] = TRUE;
+      $this->options['alter']['path'] = $entity->getSystemPath('delete-form');
+      $this->options['alter']['query'] = drupal_get_destination();
+
+      $text = !empty($this->options['text']) ? $this->options['text'] : t('delete');
+      return $text;
+    }
+  }
+
+}
---
+++ lib/Drupal/entity_views/Plugin/views/field/LinkEdit.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\entity_views\Plugin\views\field\LinkEdit.
+ */
+
+namespace Drupal\entity_views\Plugin\views\field;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to present a link to entity edit.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @PluginID("entity_link_edit")
+ */
+class LinkEdit extends Link {
+
+  /**
+     * Prepares the link to edit a entity.
+     *
+     * @param \Drupal\Core\Entity\EntityInterface $entity
+     *   The node entity this field belongs to.
+     * @param \Drupal\views\ResultRow $values
+     *   The values retrieved from the view's result set.
+     *
+     * @return string
+     *   Returns a string for the link text.
+     */
+  protected function renderLink($entity, ResultRow $values) {
+    /**
+     * @var \Drupal\Core\Entity\EntityInterface $entity
+     */
+    if ($entity && $entity->access('update')) {
+      $this->options['alter']['make_link'] = TRUE;
+      $this->options['alter']['path'] = $entity->getSystemPath('edit-form');
+      $this->options['alter']['query'] = drupal_get_destination();
+
+      $text = !empty($this->options['text']) ? $this->options['text'] : t('Edit');
+      return $text;
+    }
+  }
+
+}
